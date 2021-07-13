0945fd215250c78594753ab3d0af9b735926dbf1
batnat280 <69560016+batnat280@users.noreply.github.com>
2021-07-13 15:32:32 -0700

    Add files via upload

---
+++ cmakeFiles/cmake-build-debug/CMakeFiles/spaceGame.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/Button.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/Button.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/Collision.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/Collision.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/Enemy.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/Enemy.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/Entity.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/Entity.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/Fonts.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/Fonts.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/Game.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/Game.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/Health.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/Health.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/Laser.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/Laser.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/LinkedList.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/LinkedList.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/MainGame.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/MainGame.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/MainMenu.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/MainMenu.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/Rectangle.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/Rectangle.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/Score.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/Score.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/Stack.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/Stack.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/States.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/States.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/WindowDialog.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/WindowDialog.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/main.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/main.cpp.obj"
+  "C:/Users/Nat/CLionProjects/spaceGame_menu/player.cpp" "C:/Users/Nat/CLionProjects/spaceGame_menu/cmake-build-debug/CMakeFiles/spaceGame.dir/player.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/SFML/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmakeFiles/cmake-build-debug/CMakeFiles/spaceGame.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spaceGame.dir/Button.cpp.obj"
+  "CMakeFiles/spaceGame.dir/Collision.cpp.obj"
+  "CMakeFiles/spaceGame.dir/Enemy.cpp.obj"
+  "CMakeFiles/spaceGame.dir/Entity.cpp.obj"
+  "CMakeFiles/spaceGame.dir/Fonts.cpp.obj"
+  "CMakeFiles/spaceGame.dir/Game.cpp.obj"
+  "CMakeFiles/spaceGame.dir/Health.cpp.obj"
+  "CMakeFiles/spaceGame.dir/Laser.cpp.obj"
+  "CMakeFiles/spaceGame.dir/LinkedList.cpp.obj"
+  "CMakeFiles/spaceGame.dir/MainGame.cpp.obj"
+  "CMakeFiles/spaceGame.dir/MainMenu.cpp.obj"
+  "CMakeFiles/spaceGame.dir/Rectangle.cpp.obj"
+  "CMakeFiles/spaceGame.dir/Score.cpp.obj"
+  "CMakeFiles/spaceGame.dir/Stack.cpp.obj"
+  "CMakeFiles/spaceGame.dir/States.cpp.obj"
+  "CMakeFiles/spaceGame.dir/WindowDialog.cpp.obj"
+  "CMakeFiles/spaceGame.dir/main.cpp.obj"
+  "CMakeFiles/spaceGame.dir/player.cpp.obj"
+  "libspaceGame.dll.a"
+  "spaceGame.exe"
+  "spaceGame.exe.manifest"
+  "spaceGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spaceGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
